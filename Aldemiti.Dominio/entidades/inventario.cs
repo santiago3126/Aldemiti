b5af2c70c92595c766bd1cc38ca54271f9e65605
Your Namejuan <you@example.comfuerzanaval10@gmail.com>
2021-11-19 20:41:04 -0500

    entidad inventario

---
+++ Aldemiti.Dominio/entidades/inventario.cs
@@ -0,0 +1,16 @@
+namespace MiprimaraApp.Dominio
+{
+    public class Inventario
+    {
+        public string nombre {get; set;}
+        public string fecha_de_compra {get; set;}
+        public string fecha_de_vencimiento{get; set;}
+        public float valor_compra{get; set;}
+        public float valor_venta{get; set;}
+        public string descripcion{get;set;}
+        public int cantidad {get;set;}
+        public string codigo{get;set;}
+        public string proveedor{get;set;}
+        
+    }
+}
