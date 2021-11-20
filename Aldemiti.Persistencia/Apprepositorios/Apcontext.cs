96bbc131e0d6710a009eee4ded8ff884d426762e
Your Namejuan <you@example.comfuerzanaval10@gmail.com>
2021-11-19 21:05:47 -0500

    Conexion base datos

--- Aldemiti.Persistencia/Apprepositorios/Apcontext.cs
+++ Aldemiti.Persistencia/Apprepositorios/Apcontext.cs
@@ -6,7 +6,21 @@ namespace Aldemiti.Persistencia
      public class AppContext : DbContext
      {
          public DbSet<inventario> Inventario {get; set;}
-         
 
+         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Initial Catalog=Hospital2207208; Data Source=HOMELUNA; Integrated Security=true");
+                            //.UseSqlServer("Data Source = (localdb)\\HOMELUNA; Initial Catalog =Hospital2207208");  
+                            //.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog =Hospital2207208");
+          
+                        /*
+                        initial catalog: nombre de la base de datos
+                        data source: nombre del servidor de la base de datos
+                        integrated security: seguridad con la misma configuracion del inicio de sesion
+                        */
+            }
+        }
      }
 }
